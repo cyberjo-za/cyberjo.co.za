5c810589a01df2c725021f1c95691736def49b7a
Jacques <oostenjac@gmail.com>
2025-08-18 22:48:28 +0200

    Fix JS issue.

--- js/scripts.js
+++ js/scripts.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         daysEl.textContent = String(days).padStart(2, '0');
         hoursEl.textContent = String(hours).padStart(2, '0');
-       (minutes).padStart(2, '0');
+        minutesEl.textContent = String(minutes).padStart(2, '0'); // Corrected line
         secondsEl.textContent = String(seconds).padStart(2, '0');
     };
 
